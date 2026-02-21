0074f77b2f7bef68a27b0b86b9af1f961f9f7a5e
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2026-02-21 17:43:05 +0530

    http server added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/veetmoradiya3628/go-shop/internal/config"
 	"github.com/veetmoradiya3628/go-shop/internal/database"
 	"github.com/veetmoradiya3628/go-shop/internal/logger"
+	"github.com/veetmoradiya3628/go-shop/internal/server"
 )
 
 func main() {
@@ -24,5 +34,36 @@ func main() {
 	defer mainDB.Close()
 	gin.SetMode(cfg.Server.GinMode)
 
-	log.Info().Msg("starting server")
+	srv := server.New(cfg, db, log)
+
+	router := srv.SetupRoutes()
+
+	httpServer := &http.Server{
+		Addr:         fmt.Sprintf(":%s", cfg.Server.Port),
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		log.Info().Str("port", cfg.Server.Port).Msg("starting http server")
+		if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal().Err(err).Msg("failed to start http server")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Info().Msg("shutting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	if err := httpServer.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Msg("failed to shutdown http server")
+	}
+
+	log.Info().Msg("shutting down database")
+
 }
